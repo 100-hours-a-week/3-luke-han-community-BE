79b43cc8a43d0ab59d6ff069e7402052abe76dc2
JaeSeoHan <hanahyun1@gmail.com>
2025-10-25 10:58:49 +0900

    feat: 개인정보처리방치미 약관페이지 구현

--- src/main/java/boot/kakaotech/communitybe/term/TermController.java
+++ src/main/java/boot/kakaotech/communitybe/term/TermController.java
@@ -1,15 +1,19 @@
 package boot.kakaotech.communitybe.term;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class TermController {
 
     @GetMapping("/terms")
     public String getTerms() {
         return "terms";
+    }
+
+    @GetMapping("/privacy")
+    public String getPrivacy() {
+        return "privacy";
     }
 
 }
