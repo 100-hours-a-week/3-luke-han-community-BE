5a5e8f67e522df04532d156ab29cbd2566b07273
JaeSeoHan <hanahyun1@gmail.com>
2025-11-10 17:17:29 +0900

    feat: PasswordEncoder 구현

--- src/main/java/boot/kakaotech/communitybe/auth/service/AuthServiceImpl.java
+++ src/main/java/boot/kakaotech/communitybe/auth/service/AuthServiceImpl.java
@@ -1,6 +1,7 @@
 package boot.kakaotech.communitybe.auth.service;
 
 import boot.kakaotech.communitybe.auth.dto.SignupRequest;
+import boot.kakaotech.communitybe.common.encoder.PasswordEncoder;
 import boot.kakaotech.communitybe.user.entity.User;
 import boot.kakaotech.communitybe.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -18,6 +19,20 @@ public class AuthServiceImpl implements AuthService {
 
     private final UserRepository userRepository;
 
+    private final PasswordEncoder passwordEncoder;
+
+    /**
+     * 회원가입 서비스 메서드
+     *
+     * 1. 회원가입 요청으로 들어온 DTO validation
+     * 2. 이미지 S3용 키 발급
+     * 3. 새 유저 생성
+     * 4. 저장
+     * 5. presigned url 발급 및 반환
+     *
+     * @param request
+     * @return
+     */
     @Override
     @Transactional
     public String signup(SignupRequest request) {
@@ -25,11 +40,12 @@ public class AuthServiceImpl implements AuthService {
 
         // TODO: 밸리데이션 함수
 
+        // TODO: 키 만드는거 메서드로 빼기
         String key = "user:" + request.getEmail() + ":" + UUID.randomUUID() + ":" + request.getProfileImageName();
 
         User user = User.builder()
                 .email(request.getEmail())
-                .password("" /* TODO: passwordEncoder */)
+                .password(passwordEncoder.encode(request.getPassword()))
                 .nickname(request.getNickname())
                 .profileImageKey(key)
                 .createdAt(LocalDateTime.now())
---
+++ src/main/java/boot/kakaotech/communitybe/common/config/EncoderConfig.java
@@ -0,0 +1,15 @@
+package boot.kakaotech.communitybe.common.config;
+
+import boot.kakaotech.communitybe.common.encoder.PasswordEncoder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class EncoderConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new PasswordEncoder();
+    }
+
+}
